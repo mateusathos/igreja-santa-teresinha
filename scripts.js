a94bf7082bae938242c1eb94fbabf4fa537259fc
Athos <mateusathos1707@gmail.com>
2025-10-09 13:33:06 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (btn && menu) {
           btn.addEventListener("click", () => {
             menu.classList.toggle("menu-open");
+            btn.classList.toggle("open");
           });
         }
       })
